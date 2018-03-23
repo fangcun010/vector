bf7b6aa4b83cb1109df9e46dc26f3094f08c96ce
fangcun <fangcun@stepgame.net>
2018-03-23 18:22:40 +0800

    Edit

--- Vector.h
+++ Vector.h
@@ -4,20 +4,20 @@
 #define VECTOR_H_INCLUDED
 
 //BASE_SIZE只可以是偶数
-#define VECTOR_BASE_SIZE				64
+#define VECTOR_BASE_SIZE					    64
 
 typedef struct tagVector
 {
-	void **pArray;												//指针数组
-	int nMemorySize;											//内存大小
-	int nCount;													//元素数量
+	void **pArray;						    //指针数组
+	int nMemorySize;					    //内存大小
+	int nCount;						    //元素数量
 } Vector;
 
-Vector 			*vtCreate();									//创建向量
+Vector				*vtCreate();			    //创建向量
 void				vtDestory(Vector *pVt);
-void				vtAddBack(Vector *pVt,void *pNode);		//添加
-void 				*vtRemove(Vector *pVt,int index);		//删除
-void				*vtGet(Vector *pVt,int index);			//检索
-int				vtCount(Vector *pVt);						//元素数量
+void				vtAddBack(Vector *pVt,void *pNode); //添加
+void 				*vtRemove(Vector *pVt,int index);   //删除
+void				*vtGet(Vector *pVt,int index);	    //检索
+int				vtCount(Vector *pVt);		    //元素数量
 
 #endif // VECTOR_H_INCLUDED
